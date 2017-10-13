5ec77051900fb28900eefe6aa7c4c3c7324be280
N3verL4nd <lgh1992314@qq.com>
2017-10-13 15:06:34 +0800

    add solutions

--- src/test/java/T.java
+++ src/test/java/T.java
@@ -1,20 +1,8 @@
 import org.junit.Test;
 
-class Demo {
-    private String name = get();
-    {
-        System.out.println("非静态代码块");
-    }
-
-    public String get() {
-        System.out.println("get");
-        return "lgh";
-    }
-}
-
 public class T {
     @Test
     public void test1() {
-        Demo demo = new Demo();
+        System.out.println();
     }
 }
