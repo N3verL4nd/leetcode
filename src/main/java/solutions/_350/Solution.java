9b16da5bcf94a3839740a6beba3304a50af8cbd5
liguanghui02 <liguanghui02@meituan.com>
2020-12-25 10:46:29 +0800

    add

---
+++ src/main/java/solutions/_350/Solution.java
@@ -0,0 +1,79 @@
+package solutions._350;
+
+import java.util.*;
+
+class Solution {
+    public int[] intersect(int[] nums1, int[] nums2) {
+        if (nums1.length < nums2.length) {
+            int[] temp = nums1;
+            nums1 = nums2;
+            nums2 = temp;
+        }
+        List<Integer> list = new ArrayList<>();
+        Map<Integer, Integer> map = new HashMap<>(nums1.length);
+        for (int i : nums1) {
+            if (map.containsKey(i)) {
+                Integer value = map.get(i);
+                map.put(i, value + 1);
+                continue;
+            }
+            map.put(i, 1);
+        }
+//        System.out.println(map);
+
+        for (int i : nums2) {
+            if (map.containsKey(i)) {
+                Integer value = map.get(i);
+                if (value >= 1) {
+                    value--;
+                    map.put(i, value);
+                    list.add(i);
+                }
+            }
+        }
+
+        int[] result = new int[list.size()];
+        for (int i = 0; i < result.length; i++) {
+            result[i] = list.get(i);
+        }
+        return result;
+    }
+
+
+    public static void main(String[] args) {
+        System.out.println(Arrays.toString(new Solution().intersect(new int[]{1, 2, 2, 1}, new int[]{2, 2})));
+    }
+}
+
+
+/*
+
+给定两个数组，编写一个函数来计算它们的交集。
+
+ 
+
+示例 1：
+
+输入：nums1 = [1,2,2,1], nums2 = [2,2]
+输出：[2,2]
+示例 2:
+
+输入：nums1 = [4,9,5], nums2 = [9,4,9,8,4]
+输出：[4,9]
+ 
+
+说明：
+
+输出结果中每个元素出现的次数，应与元素在两个数组中出现次数的最小值一致。
+我们可以不考虑输出结果的顺序。
+进阶：
+
+如果给定的数组已经排好序呢？你将如何优化你的算法？
+如果 nums1 的大小比 nums2 小很多，哪种方法更优？
+如果 nums2 的元素存储在磁盘上，内存是有限的，并且你不能一次加载所有的元素到内存中，你该怎么办？
+
+来源：力扣（LeetCode）
+链接：https://leetcode-cn.com/problems/intersection-of-two-arrays-ii
+著作权归领扣网络所有。商业转载请联系官方授权，非商业转载请注明出处。
+
+ */
