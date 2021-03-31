97c2f292e3b8a5f71997f19a7c7de97e7015dea6
liguanghui02 <liguanghui02@meituan.com>
2021-03-31 17:08:16 +0800

    fix

--- src/main/java/concurrent/ReentrantLockTest.java
+++ src/main/java/concurrent/ReentrantLockTest.java
@@ -15,16 +15,29 @@ public class ReentrantLockTest {
             lock.lock();
 
             try {
-                try {
-                    TimeUnit.SECONDS.sleep(10000);
-                } catch (InterruptedException e) {
-                    e.printStackTrace();
-                }
+                System.out.println("lock1");
+                TimeUnit.SECONDS.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             } finally {
                 lock.unlock();
             }
 
-        }).start();
+        }, "thread-1").start();
+
+        new Thread(() -> {
+            lock.lock();
+
+            try {
+                System.out.println("lock2");
+                TimeUnit.SECONDS.sleep(2000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            } finally {
+                lock.unlock();
+            }
+
+        }, "thread-2").start();
 
         try {
             TimeUnit.SECONDS.sleep(1);
@@ -33,8 +46,13 @@ public class ReentrantLockTest {
         }
 
         lock.lock();
+
+
         try {
-            System.out.println("lock");
+            System.out.println("lock3");
+            TimeUnit.SECONDS.sleep(20);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         } finally {
             lock.unlock();
         }
